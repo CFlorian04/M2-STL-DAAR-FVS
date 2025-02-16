4662351c84ad1d3950a75432868abb816a7e9f61
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2025-02-16 21:16:13 +0100

    Version 1.0

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -1,21 +1,93 @@
 package algorithms;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 
 public class DefaultTeam {
 
-  public ArrayList<Point> calculFVS(ArrayList<Point> points, int edgeThreshold) {
-    ArrayList<Point> fvs = new ArrayList<Point>();
+    public Evaluation evaluation = new Evaluation();
 
-    //REMOVE >>>>>
-    for(int i=0;i<5*points.size()/6;i++){
-      fvs.add(points.get(i));
+    public ArrayList<Point> calculFVS(ArrayList<Point> points, int edgeThreshold) {
+        ArrayList<Point> fvs = new ArrayList<>(points);
+        HashSet<Point> aRetirer = new HashSet<>();
+        PriorityQueue<Point> queue = new PriorityQueue<>();
+        int temp_delete_size = -1;
+
+        ArrayList<Point> fvsTemp = new ArrayList<>();
+        int iteration = 0;
+        int max_iteration = 1;
+
+        while (iteration < max_iteration) {
+            iteration++;
+
+            queue = new PriorityQueue<>(Comparator.comparingInt(p -> evaluation.neighbor(p, fvs, edgeThreshold).size()));
+            fvsTemp = new ArrayList<>(fvs);
+            fvsTemp.removeAll(aRetirer);
+
+            for (Point p : points) {
+                if (evaluation.neighbor(p, points, edgeThreshold).size() < 2) {
+                    aRetirer.add(p);
+                } else {
+                    queue.add(p);
+                }
+            }
+
+            while (!queue.isEmpty()) {
+                Point minPoint = queue.poll();
+
+                if (aRetirer.contains(minPoint)) {
+                    continue;
+                }
+
+                fvs.remove(minPoint);
+                if (!evaluation.isValid(points, fvs, edgeThreshold)) {
+                    fvs.add(minPoint);
+                } else {
+                    aRetirer.add(minPoint);
+                }
+            }
+
+        }
+
+
+
+//        while (aRetirer.size() > temp_delete_size) {
+//            temp_delete_size = aRetirer.size();
+//            aRetirer = new HashSet<>(upgrade(points, fvs, edgeThreshold, new ArrayList<>(aRetirer)));
+//        }
+
+        fvs.removeAll(aRetirer);
+        return fvs;
     }
-    //<<<<< REMOVE
 
-    return fvs;
-  }
+//    private ArrayList<Point> upgrade(ArrayList<Point> points, ArrayList<Point> fvs, int edgeThreshold, ArrayList<Point> removeList) {
+//        ArrayList<Point> upgr_fvs = new ArrayList<Point>(fvs);
+//
+//        for (int p = 0; p < fvs.size(); p++) {
+//            for (int q = p + 1; q < fvs.size(); q++) {
+//                Point p1 = fvs.get(p);
+//                Point p2 = fvs.get(q);
+//
+//                upgr_fvs.remove(p1);
+//                upgr_fvs.remove(p2);
+//
+//                //int vl = upgr_fvs.size();
+//
+//                for (Point x : points) {
+//                    if (!upgr_fvs.contains(x)) {
+//                        upgr_fvs.add(x);
+//                        if (evaluation.isValid(points, new ArrayList<>(upgr_fvs), edgeThreshold)) {
+//                            //removeList.add(x);
+//                            return removeList;
+//                        }
+//                        upgr_fvs.remove(x);
+//                    }
+//                }
+//
+//                upgr_fvs.add(p1);
+//                upgr_fvs.add(p2);
+//            }
+//        }
+//        return removeList;
+//    }
 }
